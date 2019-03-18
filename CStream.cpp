e793dc31252975d95c90d7b69342edf92fe30e7c
Chirayu <chirayu@kr.om>
2019-03-18 14:42:37 +0000

    Operator overloaded

--- CStream.cpp
+++ CStream.cpp
@@ -1,7 +1,6 @@
 #include "CStream.h"
 
 
-
 CStream::CStream()
 {
 }
@@ -9,4 +8,17 @@ CStream::CStream()
 
 CStream::~CStream()
 {
+}
+
+Cstream Cstream ::operator+(const CStream &other)
+{
+    this->flow_rate +=  other.flow_rate;
+    //return newstream;
+}
+
+CStream CStream::operator-(const CStream &other)
+{
+    CStream newstream;
+    this->flow_rate -= other.flow_rate;
+    return newstream;
 }
