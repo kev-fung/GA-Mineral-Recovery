8bc45ed1c04345ed694aaff36bfa80fefe2542de
mattiaguerri <mattguerri1@gmail.com>
2019-03-19 17:35:21 +0000

    minor fix

--- src/mainMattia.cpp
+++ src/mainMattia.cpp
@@ -14,10 +14,13 @@ int main()
 	int numCircuits = 4;
 	int sizeVec = 21;
 	int maxIte = 1;
+	int indmax = 0;
+	int max = 0;
 
 	srand (time(NULL));
 
 	vector<int> circuit(sizeVec,0);
+	vector<int> best_circuit(sizeVec,0);
 	vector<vector<int>> circuits(numCircuits, vector<int>(sizeVec,0));
 
 	vector<double> fitVec(numCircuits,0);
@@ -26,26 +29,48 @@ int main()
 	{
 		generateCircuit(circuit);
 		circuits[i] = circuit;
-		fitVec[i] = i*2;
+		fitVec[i] = i*(20);
 	}
 
 
     // Check the vectors.
- 	cout << "what we put in the list" << endl;
+ 	cout << "Input Vectors" << endl;
 	for(int i = 0; i < 4; i++) {
 		for(int j = 0; j < size; j++) {
 			cout << circuits[i][j] << " "; 
 		}
-		cout << "fitness: " << fitVec[i] << endl;
+		cout << "   Fitness: " << fitVec[i] << endl;
 	}
 	cout << endl;
+
+
+	// find index of max fitness
+	for (int i=0; i<numCircuits; i++)
+	{
+		if (fitVec[i] > max)
+			{
+			max = fitVec[i];
+			indmax = i;
+			}
+	}
+	best_circuit = circuits[indmax];
 
 
 	geneticAlgo(circuits, fitVec, maxIte);
 
 
+    // Test genetic algo is working.
+	bool pass = true;
+	for (int i = 0; i<sizeVec; i++)
+	{
+		if (circuits[0][i] != best_circuit[i]) pass = false;
+	}
+	cout << endl << "PASS is = " << pass << endl << endl;
+
+
+    // Print the output.
+    cout << "Output Vectors" << endl;
 	for (int i = 0; i<numCircuits;i++){
-		cout << "circuit " << i << endl;
 		for (int j =0; j<size; j++){
 		cout << circuits[i][j] << " ";
 		}
