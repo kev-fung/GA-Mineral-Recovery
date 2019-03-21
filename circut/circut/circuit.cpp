ecefa44e577c5144729643055a5caa1ee9c4f826
dapolak <deirdree.polak@gmail.com>
2019-03-21 13:48:47 +0000

    merge fixes

--- circut/circut/circuit.cpp
+++ circut/circut/circuit.cpp
@@ -39,9 +39,9 @@ double Circuit::Evaluate_Circuit(std::vector<int> circuit_vector, double toleran
 	/*for (int i = 0; i > 100; i++)
 	{*/
 		// Fill up our unit_list (vector of unit objects) from circuit_vector:
-//#pragma omp parallel for
+
 	int feed_id = circuit_vector[0];
-		
+#pragma omp parallel for		
 		for (int i = 0; i < num_units; i++)
 		{
 			cout << "First for loop: " << omp_get_thread_num() << "\t";
@@ -62,7 +62,7 @@ double Circuit::Evaluate_Circuit(std::vector<int> circuit_vector, double toleran
 			iter++;
 
 			// Calculate Tail and Conc streams of all units for this time step!
-//#pragma omp parallel for
+#pragma omp parallel for
 			for (int i = 0; i < num_units; i++)
 			{
 				unit_list[i].output_con_tail();
@@ -73,7 +73,7 @@ double Circuit::Evaluate_Circuit(std::vector<int> circuit_vector, double toleran
 			//unit_list[0].feed.set_stream(circuit_feed);		// [10, 100]
 			
 
-//#pragma omp parallel for
+#pragma omp parallel for
 			for (int i = 0; i < num_units + 2; i++)			// INCLUDING END STREAMS!
 			{
 				unit_list[i].store_feed();
@@ -82,7 +82,7 @@ double Circuit::Evaluate_Circuit(std::vector<int> circuit_vector, double toleran
 			unit_list[feed_id].feed.set_stream(circuit_feed);		// [10, 100]
 
 			// Send the Tail and Conc streams to their destination units!
-//#pragma omp parallel for
+#pragma omp parallel for
 			for (int i = 0; i < num_units; i++)
 			{
 				conc_ID = unit_list[i].conc_id;
@@ -97,7 +97,7 @@ double Circuit::Evaluate_Circuit(std::vector<int> circuit_vector, double toleran
 			// Work out the relative tolerances!
 			max_rel_tol = -1e9;
 
-//#pragma omp parallel for
+#pragma omp parallel for
 			for (int i = 0; i < num_units; i++)
 			{
 				rel_tol = unit_list[i].rel_tol_calc();
@@ -106,32 +106,27 @@ double Circuit::Evaluate_Circuit(std::vector<int> circuit_vector, double toleran
 					max_rel_tol = rel_tol;
 				}
 			}
-			tot_valuable = unit_list[num_units].feed.M[0];
-			tot_waste = unit_list[num_units].feed.M[1];
-
-			fitness = (tot_valuable*valuable_price) + (tot_waste*waste_cost);
-
-			cout << iter << " " << max_rel_tol << " " << fitness << endl;
+			
 			rtol = max_rel_tol;
 
 		} while (rtol > tolerance && iter < max_iter);
 	//}
-	//double end = omp_get_wtime(); //end time measurement
-	//time += end - start;
+	double end = omp_get_wtime(); //end time measurement
+	time = end - start;
 
-	//std::cout << "average time of computation is: " << time/100 << "\n";
+	std::cout << "average time of computation is: " << time << "\n";
 
 	if (iter == max_iter)
 		return circuit_feed[1] * waste_cost;
 
-	cout << "iter: " << iter << endl;
+	//cout << "iter: " << iter << endl;
 	// Calculate fitness value based on economical value of concentration unit
 	tot_valuable = unit_list[num_units].feed.M[0];
 	tot_waste = unit_list[num_units].feed.M[1];
 
 	fitness = (tot_valuable*valuable_price) + (tot_waste*waste_cost);
 
-	cout << "fitness value is " << fitness << endl;
+	//cout << "fitness value is " << fitness << endl;
 	return fitness;
 }
 
