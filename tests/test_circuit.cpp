ba23f5d10b42344cce53ed26ad976237e41ef2a9
ckhimji <43985735+ckhimji@users.noreply.github.com>
2019-03-22 10:45:15 +0000

    debugged test_circuit.cpp

--- tests/test_circuit.cpp
+++ tests/test_circuit.cpp
@@ -11,25 +11,25 @@ int main()
 	// Test 1
 	vector<int> example1 = {0, 4, 3, 2, 0, 5, 4, 4, 6, 2, 1};
 
-	//// Test 2
-	vector<int> example2 = { 0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9, 10, 11, 10, 11, 10, 11, 10, 11 };
+	////// Test 2
+	//vector<int> example2 = { 0, 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 8, 9, 10, 11, 10, 11, 10, 11, 10, 11 };
 
-	//// Test 3
-	vector<int> example3 = { 0, 1, 11, 2, 11, 3, 11, 4, 11, 5, 11, 6, 11, 7, 11, 8, 11, 9, 11, 10, 11 };
+	////// Test 3
+	//vector<int> example3 = { 0, 1, 11, 2, 11, 3, 11, 4, 11, 5, 11, 6, 11, 7, 11, 8, 11, 9, 11, 10, 11 };
 
 	// HOW TO USE CLASS:
 	// Make new circuit object:
 	// Note: circ() uses default settings of price and circuit feeds
 	// The contructor below allows you to specify the pricings and the circuit feeds
 	Circuit circ1;
-	Circuit circ2;
-	Circuit circ3;
+	//Circuit circ2;
+	//Circuit circ3;
 
 	double tol = 1e-6;
 	int max_iter = 1000;
 	double fitness1 = circ1.Evaluate_Circuit(example1, tol, max_iter);
-	double fitness2 = circ2.Evaluate_Circuit(example2, 0.0, 1);
-	double fitness3 = circ3.Evaluate_Circuit(example3, 0.0, 1);
+	//double fitness2 = circ2.Evaluate_Circuit(example2, 0.0, 1);
+	//double fitness3 = circ3.Evaluate_Circuit(example3, 0.0, 1);
 
 	//test1
 	bool pass = true;
